fdef5540f6da2994d27029f23d01ff2691e4c1ae
amogus <cernavskiskristofers@gmail.com>
2023-12-05 16:08:47 +0200

    thrusert stuff
    thruster stuff

--- Garden Hose/Game/World/Entities/Physical/PhysicalEntityPartSprite.cs
+++ Garden Hose/Game/World/Entities/Physical/PhysicalEntityPartSprite.cs
@@ -28,23 +28,99 @@ internal class PhysicalEntityPartSprite
 
     internal Color ColorMask
     {
-        get => Sprite.Mask;
-        set => Sprite.Mask = value;
+        get
+        {
+            if (Sprite != null)
+            {
+                return Sprite.Mask;
+            }
+
+            return _colorMask;
+        }
+        set
+        {
+            if (Sprite != null)
+            {
+                Sprite.Mask = value;
+            }
+            else
+            {
+                _colorMask = value;
+            }
+        }
     }
 
     internal float Opacity
     {
-        get => Sprite.Opacity; 
-        set => Sprite.Opacity = value;
+        get
+        {
+            if (Sprite != null)
+            {
+                return Sprite.Opacity;
+            }
+
+            return _opacity;
+        }
+        set
+        {
+            if (Sprite != null)
+            {
+                Sprite.Opacity = value;
+            }
+            else
+            {
+                _opacity = value;
+            }
+        }
     }
 
     internal float Brightness
     {
-        get => Sprite.Brightness; 
-        set => Sprite.Brightness = value;
+        get
+        {
+            if (Sprite != null)
+            {
+                return Sprite.Brightness;
+            }
+
+            return _brightness;
+        }
+        set
+        {
+            if (Sprite != null)
+            {
+                Sprite.Brightness = value;
+            }
+            else
+            {
+                _brightness = value;
+            }
+        }
     }
 
-    internal SpriteEffect Effect
+    internal SpriteEffect Effect 
+    {
+        get
+        {
+            if (Sprite != null)
+            {
+                return Sprite.Effects;
+            }
+
+            return _effects;
+        }
+        set
+        {
+            if (Sprite != null)
+            {
+                Sprite.Effects = value;
+            }
+            else
+            {
+                _effects = value;
+            }
+        }
+    }
 
     internal string AssetName { get; private init; }
 
@@ -52,10 +128,10 @@ internal class PhysicalEntityPartSprite
     // Private fields.
     private SpriteItem _sprite;
 
-    private SpriteEffect _effect;
-    private float _brightness;
-    private float _opacity;
-    private Color _colorMask;
+    private SpriteEffect _effects = SpriteEffects.None;
+    private float _brightness = 1f;
+    private float _opacity = 1f;
+    private Color _colorMask = Color.White;
     
 
     // Constructors.
@@ -95,5 +171,10 @@ internal class PhysicalEntityPartSprite
         {
             throw new InvalidOperationException($"Asset \"{AssetName}\" couldn't be loaded since it wasn't found.");
         }
+
+        Sprite.Mask = _colorMask;
+        Sprite.Opacity = _opacity;
+        Sprite.Brightness = _brightness;
+        Sprite.Effects = _effects;
     }
 }
--- Garden Hose/Game/World/Entities/Ship/ThrusterPart.cs
+++ Garden Hose/Game/World/Entities/Ship/ThrusterPart.cs
@@ -11,17 +11,111 @@ namespace GardenHose.Game.World.Entities.Ship;
 internal class ThrusterPart : PhysicalEntityPart
 {
     // Internal fields.
+    /* Thrusters. */
     internal bool IsThrusterOn { get; set; }
 
-    internal float TargetThrusterThrottle { get; set; }
+    internal float _targetThrusterThrottle
+    {
+        get => _targetThrusterThrottle;
+        set
+        {
+            _targetThrusterThrottle = Math.Clamp(value, 0f, 1f);
+        }
+    }
 
-    internal float CurrentThrusterThrottle { get; }
+    internal float CurrentThrusterThrottle
+    {
+        get => _currentThrusterThrottle;
+        set
+        {
+            _currentThrusterThrottle = Math.Clamp(value, 0f, 1f);
+        }
+    }
 
-    internal float ThrusterThrottleChangeSpeed { get; }
+    internal float ThrusterThrottleChangeSpeed { get; init; }
 
-    internal float ThrusterPower { get; }
+    internal float ThrusterPower { get; init; }
+
+    internal float ForceDirection { get; set; } = 0f;
+
+
+    /* Fuel. */
+    internal float Fuel
+    {
+        get => _fuel;
+        set
+        {
+            _fuel = Math.Clamp(value, 0f, MaxFuel)
+        }
+    }
+
+    internal float MaxFuel { get; set; } = DEFALT_FUEL;
+
+    internal float FuelEfficiency { get; set; } = 1f; // Lower values indicate better efficiency, range is (0;inf)
+
+    internal bool IsFuelUsed { get; set; } = false;
+
+
+    /* Events. */
+    internal event EventHandler? OutOfFuel;
+
+
+    // Private fields.
+    private float _currentThrusterThrottle = 0f;
+    private float _targetThrusterThrottle = 0f;
+    private float _fuel = DEFALT_FUEL;
+
+    private const float DEFALT_FUEL = 20_000_000f;
 
 
     // Constructors.
     internal ThrusterPart(WorldMaterial material, PhysicalEntity entity) : base(material, entity) { }
+
+
+    // Internal methods.
+    internal void Refuel() => Fuel = MaxFuel;
+
+
+    // Protected methods.
+    [TickedFunction(false)]
+    protected void ThrusterTick()
+    {
+        if (IsThrusterOn)
+        {
+            float Step = MathF.Sign(TargetThrusterThrottle - CurrentThrusterThrottle);
+            float ThrottleChange = Step * ThrusterThrottleChangeSpeed * Entity.World!.PassedTimeSeconds;
+
+            if (Math.Abs(ThrottleChange) > Math.Abs(TargetThrusterThrottle - CurrentThrusterThrottle))
+            {
+                ThrottleChange = TargetThrusterThrottle - CurrentThrusterThrottle;
+            }
+
+            CurrentThrusterThrottle += ThrottleChange;
+        }
+        else
+        {
+            CurrentThrusterThrottle -= ThrusterThrottleChangeSpeed * Entity.World!.PassedTimeSeconds;
+        }
+        
+
+        if ((CurrentThrusterThrottle != 0f) && (Fuel != 0f))
+        {
+            Vector2 ForceDirectionVector = Vector2.TransformNormal(Vector2.UnitX, Matrix.CreateRotationZ(CombinedRotation + ForceDirection));
+            Entity.ApplyForce(ForceDirectionVector * CurrentThrusterThrottle * ThrusterPower * Entity.World!.PassedTimeSeconds, Position);
+
+            if (!IsFuelUsed)
+            {
+                Fuel -= CurrentThrusterThrottle * ThrusterPower * FuelEfficiency * Entity.World.PassedTimeSeconds;
+            }
+        }
+    }
+
+
+    // Inherited methods.
+    [TickedFunction[false]]
+    internal override void ParallelTick()
+    {
+        base.ParralelTick();
+        ThrusterTick();
+    }
 }
