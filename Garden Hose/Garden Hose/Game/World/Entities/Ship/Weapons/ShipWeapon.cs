8cf93941acd4acd24bc5a71b7d40a9347767c7f6
WOODEN CHEST <86293484+WOODEN-CHEST@users.noreply.github.com>
2024-03-04 09:35:23 +0200

    a
    a

---
+++ Garden Hose/Garden Hose/Game/World/Entities/Ship/Weapons/ShipWeapon.cs
@@ -0,0 +1,29 @@
+﻿using GardenHose.Game.World.Entities.Physical;
+using GardenHose.Game.World.Entities.Physical.Collision;
+using GardenHose.Game.World.Material;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenHose.Game.World.Entities.Ship.Weapons;
+
+internal abstract class ShipWeapon : PhysicalEntityPart
+{
+    // Internal fields.
+    internal Vector2 TargetLocation { get; set; }
+
+    internal event EventHandler<WeaponFireEventArgs>? WeaponFire;
+
+
+    // Constructors.
+    internal ShipWeapon(ICollisionBound[] collisionBounds, WorldMaterial material, PhysicalEntity? entity) : base(collisionBounds, material, entity)
+    {
+    }
+
+
+    // Internal methods.
+    internal abstract void Fire();
+}
